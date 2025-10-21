5d3920e59c1f137b326f2fb50183b8fcfc0a9be5
Arnav1701 <arnav17mahajan@gmail.com>
2025-10-21 21:30:54 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,128 +1,126 @@
-/******************************************************************************
- * @project        : EE2028 Assignment 1 Program Template
- * @file           : main.c
- * @original author: CK Tham, ECE NUS
- * @modified by    : Ni Qingqing, ECE NUS
- * @brief          : Main program body
- *
- * @description    : This code is based on work originally done by CK Tham.
- *                   Modifications were made to update functionality and
- *                   adapt to current course requirements.
- ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
- * All rights reserved.</center></h2>
- *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- ******************************************************************************
- */
-
+#include "main.h"
 #include "stdio.h"
+#include "string.h"
+#include "../../Drivers/BSP/B-L4S5I-IOT01/stm32l4s5i_iot01_tsensor.h"
+#include "../../Drivers/BSP/B-L4S5I-IOT01/stm32l4s5i_iot01_hsensor.h"
+#include "../../Drivers/BSP/B-L4S5I-IOT01/stm32l4s5i_iot01_psensor.h"
+#include "../../Drivers/BSP/B-L4S5I-IOT01/stm32l4s5i_iot01_accelero.h"
+#include "../../Drivers/BSP/B-L4S5I-IOT01/stm32l4s5i_iot01_gyro.h"
 
-#define N_MAX 10
-#define X_SIZE 12
-// Enable CYCCNT register
-#define start_timer()  *((volatile int*)0xE0001000) = 0x40000001
 
-// Disable CYCCNT register
-#define stop_timer()   *((volatile int*)0xE0001000) = 0x40000000
 
-// Get value from CYCCNT register
-#define get_timer()    *((volatile int*)0xE0001004)
-
-// Necessary function to enable printf() using semihosting
-extern void initialise_monitor_handles(void);
-
-extern int iir(int N, int* b, int* a, int x_n); // asm implementation
-int iir_c(int N, int* b, int* a, int x_n); // reference C implementation
+static void UART1_Init(void);
+UART_HandleTypeDef huart1;
 
 int main(void)
 {
-	// Necessary function to enable printf() using semihosting
-	initialise_monitor_handles();
+    int seconds_count = 1;
+    int move=1;
+    float temp_data;
+    float hum_data;
+    float pres_data;
+    int game=1;
+    HAL_Init();
+    UART1_Init();
+    BSP_ACCELERO_Init();
+    BSP_GYRO_Init();
+    BSP_HSENSOR_Init();
+    BSP_TSENSOR_Init();
+    BSP_PSENSOR_Init();
 
-	//variables
-	int i;
-	int N = 9;
+    char message_print[64]; // Increased buffer size
+    snprintf(message_print, sizeof(message_print), "Entering Red Light, Green Light as Player/Enforcer\r\n");
 
-	int it1, it2, it3, it4;
-	int ans1, ans2;
+    HAL_UART_Transmit(&huart1, (uint8_t*)message_print, strlen(message_print), 0xFFFF);
+    HAL_Delay(5000);
 
-	//  NOTE: DO NOT modify the code below this line
-	// think of the values below as numbers of the form y.yy (floating point with 2 digits precision)
-	// which are scaled up to allow them to be used integers
-	// within the iir function, we divide y by 100 (decimal) to scale it down
-//	int b[N_MAX+1] = {100, 250, 360, 450, 580}; //N+1 dimensional feedforward
-//	int a[N_MAX+1] = {100, 120, 180, 230, 250}; //N+1 dimensional feedback
-//	int x[X_SIZE] = {100, 230, 280, 410, 540, 600, 480, 390, 250, 160, 100, 340};
+    snprintf(message_print, sizeof(message_print), "Entering Green Light as Enforcer\r\n");
+    HAL_UART_Transmit(&huart1, (uint8_t*)message_print, strlen(message_print), 0xFFFF);
+    while (game){
+		while (seconds_count<=10)
+		{
 
-//	int b[N_MAX+1] = {100, 250, 360, 450, 580};
-//	int a[N_MAX+1] = {100, 120, 180, 230, 250};
-//	int x[X_SIZE] = {100};
+			float accel_data[3];
+			int16_t accel_data_i16[3] = { 0 };
+			BSP_ACCELERO_AccGetXYZ(accel_data_i16);
 
-//	int b[N_MAX+1] = {100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 5};
-//	int a[N_MAX+1] = {100, 95, 85, 75, 65, 55, 45, 35, 25, 15, 5};
-//	int x[X_SIZE] = {50, 100, 150, 200, 250, 300, -300, -200, -100, 0, 100, 200};
+			snprintf(message_print, sizeof(message_print), " \r\n %d: Green Light. Can Move \r\n", seconds_count);
+			HAL_UART_Transmit(&huart1, (uint8_t*)message_print, strlen(message_print), 0xFFFF);
+			if (seconds_count%2==0){
+				temp_data = BSP_TSENSOR_ReadTemp();
+				hum_data = BSP_HSENSOR_ReadHumidity();
+				pres_data = BSP_PSENSOR_ReadPressure();
+				snprintf(message_print, sizeof(message_print), " Temperature: %f \r\n Humidity: %f \r\n Pressure: %f \r\n", temp_data, hum_data,pres_data);
+				HAL_UART_Transmit(&huart1, (uint8_t*)message_print, strlen(message_print), 0xFFFF);
 
-//	int b[N_MAX+1] = {100, 250, 360, 450, 580};
-//	int a[N_MAX+1] = {100, 120, 180, 230, 250};
-//	int x[X_SIZE] = {100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
+			}
+			HAL_Delay(1000);
 
-	int b[N_MAX+1] = {100, 250, 360, 450, 580};
-	int a[N_MAX+1] = {100, 120, 180, 230, 250};
-	int x[X_SIZE] = {100, -100, 200, -200, 300, -300, 400, -400, 0, 0, 100, -100};
+			seconds_count++;
+		}
 
-	// Call assembly language function iir for each element of x
-	for (i=0; i<X_SIZE; i++)
-	{
-		start_timer();
-		it1 = get_timer();
-		ans1 = iir(N, b, a, x[i]);
-		it2 = get_timer() - it1;  // Derive the cycle count difference
-		it3 = get_timer();
-		ans2 = iir_c(N, b, a, x[i]);
-		it4 = get_timer() - it3;  // Derive the cycle count difference
-		stop_timer();
+		float accel_const[3];
+		int16_t accel_const_i16[3] = { 0 };// array to store the x, y and z readings.
+		BSP_ACCELERO_AccGetXYZ(accel_const_i16);
+		accel_const[0] = (float)accel_const_i16[0] * (9.8/1000.0f);
+		accel_const[1] = (float)accel_const_i16[1] * (9.8/1000.0f);
+		accel_const[2] = (float)accel_const_i16[2] * (9.8/1000.0f);
 
-		printf("asm Cycles taken: %d\n", it2);
-		printf( "asm: i = %d, y_n = %d, \n", i, ans1) ;
-		printf("C Cycles taken: %d\n", it4);
-		printf( "C  : i = %d, y_n = %d, \n", i, ans2) ;
-	}
-	while (1); //halt
+		snprintf(message_print, sizeof(message_print), "Entering Red Light as Player/Enforcer\r\n");
+		HAL_UART_Transmit(&huart1, (uint8_t*)message_print, strlen(message_print), 0xFFFF);
+
+		while (seconds_count>10 && seconds_count<=20)
+			{
+			float accel_data[3];
+			int16_t accel_data_i16[3] = { 0 };			// array to store the x, y and z readings.
+			BSP_ACCELERO_AccGetXYZ(accel_data_i16);
+			seconds_count++;
+			accel_data[0] = (float)accel_data_i16[0] * (9.8/1000.0f);
+			accel_data[1] = (float)accel_data_i16[1] * (9.8/1000.0f);
+			accel_data[2] = (float)accel_data_i16[2] * (9.8/1000.0f);
+			// Format the message with the temperature value
+			snprintf(message_print, sizeof(message_print), "\r\n %d: Red Light. Cannot Move \r\n", seconds_count-11);
+			HAL_UART_Transmit(&huart1, (uint8_t*)message_print, strlen(message_print), 0xFFFF);
+			HAL_Delay(1000);
+			if (seconds_count%2==0){
+				snprintf(message_print, sizeof(message_print), "\r\n Accel X : %f \r\n Accel Y : %f \r\n Accel Z : %f \r\n", accel_data[0], accel_data[1], accel_data[2]);
+				HAL_UART_Transmit(&huart1, (uint8_t*)message_print, strlen(message_print), 0xFFFF);
+			}
+			if (accel_data[0]-accel_const[0]>=0.5|accel_data[1]-accel_const[1]>=0.5|accel_data[2]-accel_const[2]>=0.5){
+				snprintf(message_print, sizeof(message_print), "\r\n Game Over\r\n");
+				HAL_UART_Transmit(&huart1, (uint8_t*)message_print, strlen(message_print), 0xFFFF);
+				game=0;
+				}
+
+			}
+		seconds_count=0;
+    }
 }
 
-int iir_c(int N, int* b, int* a, int x_n)
-{ 	// The implementation below is inefficient and meant only for verifying your results.
+static void UART1_Init(void)
+{
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_USART1_CLK_ENABLE();
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_6;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-	static int x_store[N_MAX] = {0}; // to store the previous N values of x_n.
-	static int y_store[N_MAX] = {0}; // to store the previous values of y_n.
-
-	int j;
-	int y_n;
-
-	y_n = x_n*b[0]/a[0];
-
-	for (j=0; j<N; j++)
-	{
-		y_n+=(b[j+1]*x_store[j]-a[j+1]*y_store[j])/a[0];
-	}
-
-	for (j=N-1; j>0; j--)
-	{
-		x_store[j] = x_store[j-1];
-		y_store[j] = y_store[j-1];
-	}
-
-	x_store[0] = x_n;
-	y_store[0] = y_n;
-
-	y_n /= 100; // scaling down
-
-	return y_n;
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 115200;
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+    huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+    if (HAL_UART_Init(&huart1) != HAL_OK)
+    {
+        while(1);
+    }
 }
